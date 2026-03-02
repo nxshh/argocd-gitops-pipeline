d0bd04094b8a9ebab77d876645ece2b3340596d2
Nisarga Murthy <nisargamurthybp@gmail.com>
2026-03-03 00:15:49 +0530

    Upgrade app to version 2

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end("Version 1 - GitOps Demo");
+  res.end("Version 2 - GitOps Demo App");
 });
 
 server.listen(3000, () => {
